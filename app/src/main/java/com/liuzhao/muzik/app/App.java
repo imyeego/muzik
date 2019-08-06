8f77807212c7193314c3980f859d9335f54d5c9e
imyeego <imyeego@gmail.com>
2019-08-06 17:17:14 +0800

    cancel the tinker plugin temporarily

--- app/src/main/java/com/liuzhao/muzik/app/App.java
+++ app/src/main/java/com/liuzhao/muzik/app/App.java
@@ -1,11 +1,12 @@
 package com.liuzhao.muzik.app;
 
+import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
-import com.tencent.tinker.loader.app.ApplicationLike;
-import com.tinkerpatch.sdk.TinkerPatch;
-import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
+//import com.tencent.tinker.loader.app.ApplicationLike;
+//import com.tinkerpatch.sdk.TinkerPatch;
+//import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
 
 import org.xutils.x;
 
@@ -14,29 +15,29 @@ import org.xutils.x;
  *
  * @author liuzhao
  */
-public class App extends MultiDexApplication {
+public class App extends Application {
 
     private static App instance;
-    private ApplicationLike mApplicationLike;
+//    private ApplicationLike mApplicationLike;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
         x.Ext.init(this);
-        mApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
-        // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
-        TinkerPatch.init(mApplicationLike)
-                .reflectPatchLibrary()
-                .fetchPatchUpdate(true)
-                // 强制更新
-                .setPatchRollbackOnScreenOff(true)
-                .setPatchRestartOnSrceenOff(true)
-                .setFetchPatchIntervalByHours(3);
-
-        // 每隔3个小时(通过setFetchPatchIntervalByHours设置)去访问后台时候有更新,
-        //通过handler实现轮训的效果
-        TinkerPatch.with().fetchPatchUpdateAndPollWithInterval();
+//        mApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
+//        // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
+//        TinkerPatch.init(mApplicationLike)
+//                .reflectPatchLibrary()
+//                .fetchPatchUpdate(true)
+//                // 强制更新
+//                .setPatchRollbackOnScreenOff(true)
+//                .setPatchRestartOnSrceenOff(true)
+//                .setFetchPatchIntervalByHours(3);
+//
+//        // 每隔3个小时(通过setFetchPatchIntervalByHours设置)去访问后台时候有更新,
+//        //通过handler实现轮训的效果
+//        TinkerPatch.with().fetchPatchUpdateAndPollWithInterval();
     }
 
     public static Context getContext(){
