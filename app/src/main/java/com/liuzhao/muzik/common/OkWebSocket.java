f0291c686c4d6b54034a2b440a2b636a511b91ca
imyeego <imyeego@gmail.com>
2020-04-01 22:46:46 +0800

    websocket tool add retry feature

--- app/src/main/java/com/liuzhao/muzik/common/OkWebSocket.java
+++ app/src/main/java/com/liuzhao/muzik/common/OkWebSocket.java
@@ -13,24 +13,19 @@ import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import okio.ByteString;
 
-public class OkWebSocket {
-    public static final String TAG = "OkWebSocket";
-    private static OkWebSocket INSTANCE;
-    public static final int TIME_OUT = 3_000;
+public class OkWebsocket {
+    public static final String TAG = "OkWebsocket";
+    private static OkWebsocket INSTANCE;
     private OkHttpClient okHttpClient;
     private String url;
-    private volatile WebSocket mWebSocket;
-    private WebSocketCallback callback;
-    private WebSocketListener webSocketListener;
-    private Object lock = new Object();
-    private boolean flag = true;
-    private Request request;
+    WebSocket mWebSocket;
+    WebSocketCallback callback;
 
-    public static OkWebSocket instance() {
+    public static OkWebsocket instance() {
         if (INSTANCE == null) {
-            synchronized (OkWebSocket.class) {
+            synchronized (OkWebsocket.class) {
                 if (INSTANCE == null) {
-                    INSTANCE = new OkWebSocket();
+                    INSTANCE = new OkWebsocket();
                 }
             }
         }
@@ -38,45 +33,32 @@ public class OkWebSocket {
         return INSTANCE;
     }
 
-    private OkWebSocket() {
+    private OkWebsocket() {
         okHttpClient = new OkHttpClient.Builder()
-                .connectTimeout(TIME_OUT, TimeUnit.MILLISECONDS)
+                .connectTimeout(10_000, TimeUnit.MILLISECONDS)
                 .build();
 
     }
 
 
-    public OkWebSocket callback(WebSocketCallback callback) {
+    public OkWebsocket callback(WebSocketCallback callback) {
         this.callback = callback;
         return this;
     }
 
-    public OkWebSocket url(final String url) {
+    public OkWebsocket url(final String url) {
         this.url = url;
         return this;
     }
 
-    public OkWebSocket connect() {
+    public OkWebsocket connect() {
         return connect(url);
     }
 
-    public OkWebSocket disconnect() {
-        if (mWebSocket != null) {
-            mWebSocket.close(1000, "正常断开...");
-            retry(false);
-        }
-
-        return this;
-    }
-
-    public OkWebSocket connect(final String url) {
-        if (mWebSocket != null) {
-            Log.i(TAG, "连接已经成功建立，无需重复打开");
-        }
-        request = new Request.Builder().url(url).build();
-        webSocketListener = new EchoWebsocketListener();
-        okHttpClient.newWebSocket(request, webSocketListener);
-        Utils.executorService().execute(monitor);
+    public OkWebsocket connect(final String url) {
+        Request request = new Request.Builder().url(url).build();
+        EchoWebsocketListener socketListener = new EchoWebsocketListener();
+        okHttpClient.newWebSocket(request, socketListener);
         return this;
     }
 
@@ -109,49 +91,22 @@ public class OkWebSocket {
         public void onClosing(WebSocket webSocket, int code, String reason) {
             super.onClosing(webSocket, code, reason);
             Log.i(TAG, "onClosing: " + reason);
-            retry(true);
         }
 
         @Override
         public void onClosed(WebSocket webSocket, int code, String reason) {
             super.onClosed(webSocket, code, reason);
             Log.i(TAG, "onClosed: " + reason);
-            mWebSocket = null;
+
         }
 
         @Override
         public void onFailure(WebSocket webSocket, Throwable t, Response response) {
             super.onFailure(webSocket, t, response);
-            Log.i(TAG, "onFailure: " + t.getMessage());
-            retry(true);
+            Log.i(TAG, "onClosed: " + t.getMessage());
+
         }
     }
-
-    private void retry(boolean retry) {
-        flag = retry;
-        synchronized (lock) {
-            mWebSocket = null;
-            lock.notify();
-        }
-    }
-
-    private final Runnable monitor = () -> {
-        while (flag) {
-            synchronized (lock) {
-                try {
-                    Thread.sleep(TIME_OUT);
-                    if (mWebSocket != null) lock.wait();
-                    if (!flag) break;
-                    Log.i(TAG, "连接重试中...");
-                    okHttpClient.newWebSocket(request, webSocketListener);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-    };
 
     public interface WebSocketCallback {
         void onMessage(String message);
